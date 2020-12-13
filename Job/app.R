2f513131724d56b1179620db073960e0ffd929b4
Ruxin Liu <liurx@bu.edu>
2020-12-13 23:46:00 +0800

    Update app.R

--- Job/app.R
+++ Job/app.R
@@ -16,12 +16,16 @@ Adzuna_data <- read.csv("Adzuna_data.csv")
 Adzuna_us <- read.csv("Adzuna_us.csv")
 cite <- read.csv("jobsite.csv")
 
+website <- c("Adzuna_uk", "Adzuna_us", "The Muse")
+
 # Define UI for application that draws a histogram
+
 ui <- fluidPage(
     # Add web page title
     titlePanel(h1("Job Descriptions ", align = "center",
                   style = {'background-color:hsl(90, 85%,90%);color:green; 
                     border:4px double black'})),
+    h4(tags$a(href="https://github.com/Ruxinliu97/MA615-Final-Project", "Ruxin Liu")),
     mainPanel(
         tabsetPanel(
             tabPanel("Job Board Websites", 
@@ -37,7 +41,7 @@ ui <- fluidPage(
                          mainPanel(
                              tableOutput("view") 
                          ))) 
-
+            
             
         )
     )
@@ -53,6 +57,25 @@ server <- function(input, output) {
             view(viewdata[viewdata$Name == input$platform, c(2, 3)])
         }
         
+    })
+    output$cloud <- renderWordcloud2({
+        text_us <- Adzuna_us$description
+        docs_us <- Corpus(VectorSource(text_us))
+        
+        docs_us <- docs_us %>%
+            tm_map(removeNumbers) %>%
+            tm_map(removePunctuation) %>%
+            tm_map(stripWhitespace)
+        docs_us <- tm_map(docs_us, content_transformer(tolower))
+        docs_us <- tm_map(docs_us, removeWords, stopwords("english"))
+        
+        dtm_us <- TermDocumentMatrix(docs_us) 
+        matrix_us <- as.matrix(dtm_us) 
+        words_us <- sort(rowSums(matrix_us),decreasing=TRUE) 
+        df_us <- data.frame(word = names(words_us),freq=words_us)
+        
+        set.seed(1997) 
+        wordcloud(words = df_us$word, freq = df_us$freq, min.freq = 1, max.words=200, random.order=FALSE, rot.per=0.35,            colors=brewer.pal(8, "Dark2"))
     })
     
 }
